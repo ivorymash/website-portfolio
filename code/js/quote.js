b3b9cea9555aad89ff71ec444c034e4b7bff16dd
ivorymash <checkrflix@yahoo.com>
2020-05-11 05:07:25 +0800

    quotes

---
+++ code/js/quote.js
@@ -0,0 +1,38 @@
+var rng = Math.round(Math.random() * 10);
+  switch (rng) {
+    case 0:
+        quote = "top text"
+      break;
+    case 1:
+        quote = "please employ me"
+      break;
+    case 2:
+        quote = "all my homies eat cum"
+      break;
+    case 3:
+        quote = "ronaldinho soccer"
+      break;
+    case 4:
+        quote = "It doesnt matter what youve done, but what you do with what youve done... for others"
+      break;
+    case 5:
+        quote = "i shit mypanyt"
+      break;
+    case 6:
+        quote = "bro im just vibing"
+      break;
+    case 7:
+        quote = "doin your mom"
+      break;
+    case 8:
+        quote = "i hope no one sees me put my own penis in my ass"
+      break;
+    case 9:
+        quote = "If I eated soap, i dont eat it because I did, no I didnt❤️"
+      break;
+    case 10:
+        quote = "bottom text"
+      break;
+}
+
+document.getElementById("quote").innerHTML = quote
